36700940eba8872e7a78a2a1157c93183d620f91
eson-schulz <ethan@eschulz.com>
2015-10-24 15:38:30 -0500

    Moved location of admin for company to account

--- board/admin.py
+++ board/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from models import Post, Category, Company, User
+from .models import Post, Category, User
 
 admin.site.register(Post)
 admin.site.register(Category)
-admin.site.register(Company)
 admin.site.register(User)
