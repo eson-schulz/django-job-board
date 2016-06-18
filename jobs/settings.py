ddcbc94bff1a48c8119c96b4d65b2b66a9d5a82a
eson-schulz <ethan@eschulz.com>
2016-06-18 00:11:38 -0500

    More heroku ;(

--- jobs/settings.py
+++ jobs/settings.py
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'jobs/static'),
+    os.path.join(BASE_DIR, 'board/static'),
 )
 
 # Static file serving (heroku)
