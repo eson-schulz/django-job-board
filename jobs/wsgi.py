15fb0bac13afc90e5f8e3400a922a17014d6aa54
eson-schulz <ethan@eschulz.com>
2016-06-18 00:22:13 -0500

    Another fix for Heroku

--- jobs/wsgi.py
+++ jobs/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobs.settings")
 
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
