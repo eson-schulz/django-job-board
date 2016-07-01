9f6abe7c7ac51e1a85ee1e3f66d53773816ef16e
eson-schulz <ethan@eschulz.com>
2016-07-01 14:57:34 -0500

    Added import of stripe to make sure the server runs

--- jobs/settings.py
+++ jobs/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import dj_database_url
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+import stripe
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
